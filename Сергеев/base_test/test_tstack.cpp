899ec6b458c2c7620b49f85b512c9eb33a56887b
Alex-Sergeev <serega-al_ss2@mail.ru>
2016-11-16 15:38:32 +0300

    Тесты

--- Сергеев/base_test/test_tstack.cpp
+++ Сергеев/base_test/test_tstack.cpp
@@ -1,7 +1,58 @@
 #include "stack.h"
 #include <gtest.h>
 
-TEST(TStack, can_create_stack_with_positive_length)
+TEST(TStack, Empty_Stack_Is_Empty)
 {
-  ADD_FAILURE();
+	TStack<int> tmp;
+
+	EXPECT_EQ (1, tmp.IsEmpty());
+}
+TEST(TStack, Not_Empty_Stack_Is_Not_Empty)
+{
+	TStack<int> tmp;
+	tmp.Push(25);
+
+	EXPECT_EQ(0, tmp.IsEmpty());
+}
+TEST(TStack, Can_Push_Elem)
+{
+	TStack<int> tmp;
+
+	ASSERT_NO_THROW(tmp.Push(25));
+}
+TEST(TStack, Can_Push_Elem_In_Full_Stack)
+{
+	TStack<int> tmp;
+	for (int i = 0; i<50; i++)
+		tmp.Push(i);
+
+	ASSERT_NO_THROW(tmp.Push(50));
+}
+TEST(TStack, Can_Pop_Elem)
+{
+	TStack<int> tmp;
+	tmp.Push(25);
+
+	EXPECT_EQ(25,tmp.Pop());
+}
+TEST(TStack, Cant_Pop_Elem_From_Empty_Stack)
+{
+	TStack<int> tmp;
+
+	ASSERT_ANY_THROW(tmp.Pop());
+}
+TEST(TStack, Not_Full_Stack_Is_Not_Full)
+{
+	TStack<int> tmp;
+	tmp.Push(25);
+
+	EXPECT_EQ(0, tmp.IsFull());
+}
+TEST(TStack, Full_Stack_Is_Full)
+{
+	TStack<int> tmp;
+	for (int i=0;i<50;i++)
+		tmp.Push(25);
+
+	EXPECT_EQ(1, tmp.IsFull());
 }
