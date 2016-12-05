a3b8b66422623d41fead22546138a123fff8b61e
Alex-Sergeev <serega-al_ss2@mail.ru>
2016-12-05 19:22:24 +0300

    Изменение заставки программы

--- Сергеев/base/sample_postfix.cpp
+++ Сергеев/base/sample_postfix.cpp
@@ -10,16 +10,19 @@ int main()
   double res;
 
   setlocale(LC_ALL, "Russian");
-	  cout << "Введите африфмитическиое выражение" << endl;
-	  cin >> expression;
-	 TPostfix Postfix(expression);
-	 if (Postfix.ErrorChecking())
-	 {
-		 Postfix.ToPostfix();
-		 cout << "Арифметическое выражение: " << Postfix.GetInfix() << endl;
-		 cout << "Постфиксная форма: " << Postfix.GetPostfix() << endl;
-		 res = Postfix.Calculate();
-		 cout << res << endl;
-	 }
+  cout << "                Лабораторная работа №3" << endl;
+  cout << "          Вычисление арифметических выражений" << endl;
+  cout << endl;
+  cout << "Введите африфмитическое выражение" << endl;
+  cin >> expression;
+  TPostfix Postfix(expression);
+  if (Postfix.ErrorChecking())
+  {
+	  Postfix.ToPostfix();
+	  cout << "Арифметическое выражение: " << Postfix.GetInfix() << endl;
+	  cout << "Постфиксная форма: " << Postfix.GetPostfix() << endl;
+	  res = Postfix.Calculate();
+	  cout <<"Ответ:  "<< res << endl;
+  }
   return 0;
 }
